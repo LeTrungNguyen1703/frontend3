9443cfd815785e04e32c9a08107102dab1f0f1cf
Nguyen <nguyenc2300110@student.ctu.edu.vn>
2025-11-17 21:59:38 +0700

    Add contact management components and API integration

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <div class="jumbotron">
+      <h1 class="display-4">
+        <i class="fas fa-address-book"></i> Contact Book
+      </h1>
+      <p class="lead">Welcome to your Contact Management Application</p>
+      <hr class="my-4">
+      <p>Manage your contacts efficiently with our easy-to-use interface.</p>
+      <router-link to="/contacts" class="btn btn-primary btn-lg">
+        <i class="fas fa-users"></i> View Contacts
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+};
+</script>
+
+<style scoped>
+.jumbotron {
+  background-color: #f8f9fa;
+  border-radius: 10px;
+  padding: 3rem 2rem;
+}
+</style>
+
